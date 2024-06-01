8a7fddbaa91b64c8acb300e2fa183efb9d2d92f8
Jeisri <96050673+Jeisri@users.noreply.github.com>
2024-06-01 23:48:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
 } from 'react-router-dom';
 import Home from './components/Home'; 
 import Handson from './components/Handson';
-import Filters from './components/Filters';
 
 const App = () => {
     return (
@@ -16,7 +15,7 @@ const App = () => {
                     <Route path="/" element={<Home />} />
                     <Route path="/handson" element={<Handson />} />
                 </Routes>
-               <Filters />
+               
             </div> 
             
         </Router>
